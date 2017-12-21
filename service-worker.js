e7beeb7e5a7c3c83c3094a6fbad0330dc9e3adeb
wuhaomeng <wuhaomeng@chngalaxy.com>
2017-12-21 10:10:10 +0800

    [*]

--- service-worker.js
+++ service-worker.js
@@ -4,22 +4,22 @@
 const
   version = '2.0.3',
   CACHE = version + '::PWAsite',
-  offlineURL = '/offline/',
+  offlineURL = './offline/',
   installFilesEssential = [
-    '/',
-    '/manifest.json',
-    '/css/styles.css',
-    '/js/main.js',
-    '/js/offlinepage.js',
-    '/images/logo/logo152.png',
-    '/images/logo/logo192.png'
+    './',
+    './manifest.json',
+    './css/styles.css',
+    './js/main.js',
+    './js/offlinepage.js',
+    './images/logo/logo152.png',
+    './images/logo/logo192.png'
   ].concat(offlineURL),
   installFilesDesirable = [
-    '/favicon.ico',
-    '/images/logo/logo016.png',
-    '/images/hero/power-pv.jpg',
-    '/images/hero/power-lo.jpg',
-    '/images/hero/power-hi.jpg'
+    './favicon.ico',
+    './images/logo/logo016.png',
+    './images/hero/power-pv.jpg',
+    './images/hero/power-lo.jpg',
+    './images/hero/power-hi.jpg'
   ];
 
 // install static assets
